834abdefee415e0bfda3544afa7c820eb04826a6
philipyoo <philipyoo10@gmail.com>
2017-03-24 22:58:52 +0000

    Add solution for 0x13-0

---
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/holberton':
+  ensure  => file,
+  path    => '/tmp/holberton',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
