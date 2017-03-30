f51caee43dff2573a9c40a91ab5c30d47d4d6fa5
philipyoo <philipyoo10@gmail.com>
2017-03-30 17:42:38 +0000

    Add solution for 0x14-0

---
+++ 0x14-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Use strace to find out why Apache is returning a 500 error
+exec { 'fix apache':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
+}
