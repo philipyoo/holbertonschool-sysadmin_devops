e49577b8ce109706652749d749f2430eaf477015
philipyoo <philipyoo10@gmail.com>
2017-04-17 21:27:35 +0000

    Add solution for 0x17-1

---
+++ 0x17-api/1-export_to_CSV.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Extend the python script from exercise 0 to export data in CSV format.
+Record all taks that are owned by the employee.
+Format must be: "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+File name must be: USER_ID.csv
+"""
+import sys
+import csv
+import requests
+
+if __name__ == "__main__":
+    eid = sys.argv[1]
+    username = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(eid)).json().get("username")
+    all_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            temp = []
+            temp.extend((eid,
+                         username,
+                         task.get("completed"),
+                         task.get("title")))
+            all_tasks.append(temp)
+
+    with open("{}.csv".format(eid), 'w+') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+        writer.writerows(all_tasks)
