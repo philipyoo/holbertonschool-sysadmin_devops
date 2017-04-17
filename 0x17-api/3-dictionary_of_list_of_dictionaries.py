7bf2e2eb62384006bc2646863e795f2ded5ddb8d
philipyoo <philipyoo10@gmail.com>
2017-04-17 22:02:26 +0000

    Add solution for 0x17-3

---
+++ 0x17-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Extend the python script from exercise 0 to export data in JSON format.
+
+Records all completed tasks from all employees.
+Format must be: {"USER_ID": [ {"task": TASK_TITLE,
+                               "completed": TASK_COMPLETED_STATUS,
+                               "username": USERNAME"},
+                               {...}
+                            ]
+File name must be: todo_all_employees.json
+"""
+import json
+import requests
+
+if __name__ == "__main__":
+    users = requests.get("http://jsonplaceholder.typicode.com/users").json()
+    tasks = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+    storage = {}
+
+    for user in users:
+        eid = user.get("id")
+        username = user.get("username")
+        all_tasks = []
+
+        for task in tasks:
+            if (task.get("userId") == eid and
+                task.get("completed")):
+                temp = {}
+                temp["task"] = task.get("title")
+                temp["completed"] = task.get("completed")
+                temp["username"] = username
+                all_tasks.append(temp)
+
+        storage[eid] = all_tasks
+
+    with open("todo_all_employees.json", 'w+') as jsonfile:
+        json.dump(storage, jsonfile)
