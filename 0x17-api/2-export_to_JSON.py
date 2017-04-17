25e31fb15d3c820b22f28844047e90b43e994b2c
philipyoo <philipyoo10@gmail.com>
2017-04-17 21:47:55 +0000

    Add solution for 0x17-2

---
+++ 0x17-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Extend the python script from exercise 0 to export data in JSON format.
+
+Record all tasks that are owned by the employee.
+Format must be: {"USER_ID": [ {"task": TASK_TITLE,
+                               "completed": TASK_COMPLETED_STATUS,
+                               "username": USERNAME"},
+                               {...}
+                            ]
+File name must be: USER_ID.json
+"""
+import sys
+import json
+import requests
+
+if __name__ == "__main__":
+    eid = sys.argv[1]
+    username = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                            .format(eid)).json().get("username")
+    all_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
+
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            temp = {}
+            temp["task"] = task.get("title")
+            temp["completed"] = task.get("completed")
+            temp["username"] = username
+            all_tasks.append(temp)
+
+    with open("{}.json".format(eid), 'w+') as jsonfile:
+        json.dump({eid: all_tasks}, jsonfile)
