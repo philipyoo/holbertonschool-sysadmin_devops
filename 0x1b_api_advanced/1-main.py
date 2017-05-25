f19751c20b8f9b2980f6fabbade3cfe024309598
philipyoo <philipyoo10@gmail.com>
2017-05-25 21:12:31 +0000

    Add solution for 0x1b-1

---
+++ 0x1b_api_advanced/1-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    top_ten = __import__('1-top_ten').top_ten
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
---
+++ 0x1b_api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import requests
+
+def top_ten(subreddit):
+    """ GET subreddit top 10 hot posts """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {'user-agent': 'philsrequest'}
+    r = requests.get(url, headers=headers)
+    if (r.status_code is 404):
+        print("None")
+    elif 'data' not in r.json():
+        print("None")
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            print(post['data']['title'])
