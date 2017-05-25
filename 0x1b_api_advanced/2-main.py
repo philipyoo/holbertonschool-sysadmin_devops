fae3d50e4128b77aa4b3f7357cea9d9fe02e6b92
philipyoo <philipyoo10@gmail.com>
2017-05-25 23:04:47 +0000

    Add solution for 0x1b-2

---
+++ 0x1b_api_advanced/2-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == '__main__':
+    recurse = __import__('2-recurse').recurse
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        result = recurse(sys.argv[1])
+        if result is not None:
+            print(len(result))
+        else:
+            print("None")
---
+++ 0x1b_api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import requests
+
+def recurse(subreddit, hot_list=[], after=""):
+    """
+    GET all titles of hot articles for a given subreddit.
+    Store results in hot_list provided as default to method.
+    Requires recursive request stores.
+    """
+    if (after is None):
+        return hot_list
+
+    if (len(hot_list) == 0):
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
+    headers = {'user-agent': 'philsrequest'}
+
+    r = requests.get(url, headers=headers)
+    if (r.status_code is 404):
+        return None
+    elif 'data' not in r.json():
+        return None
+    else:
+        r = r.json()
+        for post in r['data']['children']:
+            hot_list.append(post['data']['title'])
+
+    after = r['data']['after']
+    return recurse(subreddit, hot_list, after)
