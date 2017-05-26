3cbab02b8af3bf2191e9eafc245224247b12864f
philipyoo <philipyoo10@gmail.com>
2017-05-26 23:10:07 +0000

    Edit solution for 0x1B-0

--- 0x1B-api_advanced/0-subs.py
+++ 0x1B-api_advanced/0-subs.py
@@ -7,10 +7,12 @@ def number_of_subscribers(subreddit):
     url = "http://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'user-agent': 'philsrequest'}
     r = requests.get(url, headers=headers)
-    if (r.status_code is 302):
+    if (r.status_code is 302 or r.status_code is 404):
         return 0
     r = r.json()
-    if ('subscribers' in r['data']):
+    if ('error' in r):
+        return 0
+    elif ('subscribers' in r['data']):
         return r['data']['subscribers']
     else:
         return 0
